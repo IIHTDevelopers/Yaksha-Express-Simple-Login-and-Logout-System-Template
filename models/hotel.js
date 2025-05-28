01dbbf2e77430b55f0778b1438005b8809d3b8ce
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-28 21:49:27 +0530

    Delete models directory

--- models/hotel.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    hotels: [
-        { id: 1, name: 'Hotel Sunshine', location: 'Paris', pricePerNight: 200 },
-        { id: 2, name: 'Hotel Mirage', location: 'New York', pricePerNight: 250 },
-    ]
-};
--- models/user.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    users: [
-        { id: 1, name: 'Admin', email: 'admin@example.com', passwordHash: '$2a$10$H7sRLDuY3lGhJ5vV2nTXcu9sJgy89.gAOXNh..5a0p./rNpeioxeC', age: 30 } // password = 'admin123'
-    ]
-};
