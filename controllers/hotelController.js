83299b0a2eaf8a56cd90e18f6a52dd89d14478f0
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-28 21:49:05 +0530

    Delete controllers directory

--- controllers/hotelController.js
+++
@@ -1,8 +0,0 @@
-
-// Get all hotels (Public route)
-
-// Get hotel by ID (Public route)
-
-// Create a hotel (Protected route)
-
-// Update a hotel (Protected route)
--- controllers/userController.js
+++
@@ -1,4 +0,0 @@
-const bcrypt = require('bcrypt');
-const { users } = require('../models/user');
-
-// User registration
