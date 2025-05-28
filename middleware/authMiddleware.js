0401b0823b7df5786a3e7b2f2381473976268b46
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-28 21:49:19 +0530

    Delete middleware directory

--- middleware/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcrypt');
-const { users } = require('../models/user');
-
-module.exports = (req, res, next) => {
-    // Get the authorization header
-};
--- middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    res.status(err.status || 500).json({
-        message: err.message || 'Something went wrong',
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
-    });
-};
-
-module.exports = errorHandler;
